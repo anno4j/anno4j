36de545a3b2d878b1d4a7be65cd180fa6a18eafd
Kai Schlegel <kai.schlegel@googlemail.com>
2015-06-03 15:41:34 +0200

    each object can return ntiples

--- src/main/java/com/github/anno4j/model/impl/ResourceObject.java
+++ src/main/java/com/github/anno4j/model/impl/ResourceObject.java
@@ -2,9 +2,21 @@ package com.github.anno4j.model.impl;
 
 import com.github.anno4j.Anno4j;
 import org.openrdf.model.Resource;
+import org.openrdf.model.Statement;
 import org.openrdf.model.impl.URIImpl;
+import org.openrdf.query.GraphQueryResult;
+import org.openrdf.query.MalformedQueryException;
+import org.openrdf.query.QueryEvaluationException;
+import org.openrdf.query.QueryLanguage;
+import org.openrdf.repository.Repository;
+import org.openrdf.repository.RepositoryException;
+import org.openrdf.repository.config.RepositoryConfigException;
 import org.openrdf.repository.object.ObjectConnection;
+import org.openrdf.repository.object.ObjectRepository;
 import org.openrdf.repository.object.RDFObject;
+import org.openrdf.repository.object.config.ObjectRepositoryFactory;
+import org.openrdf.repository.sail.SailRepository;
+import org.openrdf.sail.memory.MemoryStore;
 
 /**
  * Class to implement RDF in order to create a baseline for every object that we use in Anno4j.
@@ -58,5 +70,42 @@ public class ResourceObject implements RDFObject {
      */
     public String getResourceAsString() {
         return this.resource.stringValue();
+    }
+
+    public String getNTriples(){
+        StringBuilder sb = new StringBuilder();
+        try {
+            MemoryStore store = new MemoryStore();
+            Repository sailRepository = new SailRepository(store);
+            sailRepository.initialize();
+            ObjectRepository objectRepository = new ObjectRepositoryFactory().createRepository(sailRepository);
+            ObjectConnection connection = objectRepository.getConnection();
+            connection.addObject(this);
+            GraphQueryResult result = sailRepository.getConnection().prepareGraphQuery(QueryLanguage.SPARQL, "CONSTRUCT { ?s ?p ?o. } WHERE { ?s ?p ?o. } ").evaluate();
+            while (result.hasNext()) {
+                Statement item = result.next();
+
+                sb
+                        .append(item.getSubject())
+                        .append(" ")
+                        .append(item.getPredicate())
+                        .append(" ")
+                        .append(item.getObject())
+                        .append(".")
+                        .append(System.getProperty("line.separator"));
+            }
+            result.close();
+            connection.close();
+        } catch (RepositoryException e) {
+            e.printStackTrace();
+        } catch (RepositoryConfigException e) {
+            e.printStackTrace();
+        } catch (MalformedQueryException e) {
+            e.printStackTrace();
+        } catch (QueryEvaluationException e) {
+            e.printStackTrace();
+        }
+
+        return sb.toString();
     }
 }
