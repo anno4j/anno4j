3e41903e0c40183a8497ecc446cf11f9a022753a
Andreas Eisenkolb <andreas.eisenkolb@gmail.com>
2015-06-03 14:02:24 +0200

    Added language support. Added querying without the constraint parameter. Added tests for the new features.

--- src/test/java/com/github/anno4j/model/mock/TestBody.java
+++ src/test/java/com/github/anno4j/model/mock/TestBody.java
@@ -2,6 +2,7 @@ package com.github.anno4j.model.mock;
 
 import com.github.anno4j.model.Body;
 import org.openrdf.annotations.Iri;
+import org.openrdf.repository.object.LangString;
 
 /**
  * Created by schlegel on 06/05/15.
@@ -14,6 +15,17 @@ public class TestBody extends Body {
 
     @Iri("http://www.example.com/schema#value")
     private String value;
+
+    @Iri("http://www.example.com/schema#langValue")
+    private LangString langValue;
+
+    public LangString getLangValue() {
+        return langValue;
+    }
+
+    public void setLangValue(LangString langValue) {
+        this.langValue = langValue;
+    }
 
     public String getValue() {
         return value;
