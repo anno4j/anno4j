ceeaf7144ba265141523683dc7728331966b0088
Kai Schlegel <kai.schlegel@googlemail.com>
2015-09-11 14:29:33 +0200

    allow multiple targets fixes #35

--- anno4j-core/src/test/java/com/github/anno4j/model/impl/AnnotationTest.java
+++ anno4j-core/src/test/java/com/github/anno4j/model/impl/AnnotationTest.java
@@ -1,19 +1,26 @@
 package com.github.anno4j.model.impl;
 
 import com.github.anno4j.model.Annotation;
+import com.github.anno4j.model.Target;
+import com.github.anno4j.model.impl.target.SpecificResource;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openrdf.model.Resource;
 import org.openrdf.model.impl.URIImpl;
 import org.openrdf.repository.Repository;
+import org.openrdf.repository.RepositoryException;
 import org.openrdf.repository.object.ObjectConnection;
 import org.openrdf.repository.object.ObjectRepository;
 import org.openrdf.repository.object.config.ObjectRepositoryFactory;
 import org.openrdf.repository.sail.SailRepository;
 import org.openrdf.sail.memory.MemoryStore;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by schlegel on 06/05/15.
@@ -71,5 +78,59 @@ public class AnnotationTest {
 
         // Tests
         assertEquals(annotation.getResource(), result.getResource());
+    }
+
+    @Test
+    public void testSingleTarget() throws RepositoryException {
+        // Create annotation
+        Annotation annotation = new Annotation();
+
+        // Create specific resource
+        SpecificResource specificResource = new SpecificResource();
+        specificResource.setSource(new ResourceObject("http://www.somepage.org/resource1/"));
+        annotation.setTarget(specificResource);
+
+        // Persist annotation
+        connection.addObject(annotation);
+
+        // Query annotation
+        Annotation result = (Annotation) connection.getObject(annotation.getResource());
+
+        // Tests
+        assertEquals(("http://www.somepage.org/resource1/"), ((SpecificResource) result.getTarget()).getSource().getResource().toString());
+        assertEquals(1, result.getTargets().size());
+    }
+
+    @Test
+    public void testMultipleTargets() throws RepositoryException {
+        // Create annotation
+        Annotation annotation = new Annotation();
+
+        // Create specific resource1
+        SpecificResource specificResource = new SpecificResource();
+        specificResource.setSource(new ResourceObject("http://www.somepage.org/resource1/"));
+        annotation.addTarget(specificResource);
+
+        // Create specific resource2
+        SpecificResource specificResource2 = new SpecificResource();
+        specificResource2.setSource(new ResourceObject("http://www.somepage.org/resource2/"));
+        annotation.addTarget(specificResource2);
+
+
+        // Persist annotation
+        connection.addObject(annotation);
+
+        // Query annotation
+        Annotation result = (Annotation) connection.getObject(annotation.getResource());
+
+        // Tests
+        List<String> urls = new ArrayList<>();
+        for(Target target : result.getTargets()) {
+            urls.add(((SpecificResource) target).getSource().getResource().toString());
+        }
+
+        assertTrue(urls.contains("http://www.somepage.org/resource1/"));
+        assertTrue(urls.contains("http://www.somepage.org/resource2/"));
+        assertEquals(2, result.getTargets().size());
     }
 }
