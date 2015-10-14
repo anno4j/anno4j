7390f56f43b522fad9f46ce336ebf843aaa9fda3
Kai Schlegel <kai.schlegel@googlemail.com>
2015-10-14 16:00:58 +0200

    activate tests for deployment

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,14 +3,14 @@ set -ev
 
 if [ "${TRAVIS_PULL_REQUEST}" == "false" ] && [ "${TRAVIS_BRANCH}" == "develop" ]; then
    echo "Deploy develop snapshot of Anno4j";
-   mvn clean deploy  -DskipTests=true --settings settings.xml
+   mvn clean deploy --settings settings.xml
 elif [ "${TRAVIS_PULL_REQUEST}" == "false" ] && [ "${TRAVIS_BRANCH}" == "v2-develop" ]; then
    echo "Deploy develop v2 snapshot of Anno4j";
-   mvn clean deploy  -DskipTests=true --settings settings.xml
+   mvn clean deploy --settings settings.xml
 elif [ "${TRAVIS_PULL_REQUEST}" == "false" ] && [ "${TRAVIS_BRANCH}" == "master" ]; then
    echo "Release Anno4j";
-   mvn clean release:prepare release:perform -DignoreSnapshots=true -DskipTests=true -B --settings settings.xml
+   mvn clean release:prepare release:perform -DignoreSnapshots=true -B --settings settings.xml
 else
    echo "Verify Anno4j";
-   mvn clean verify -DskipTests=true --settings settings.xml
+   mvn clean verify --settings settings.xml
 fi
