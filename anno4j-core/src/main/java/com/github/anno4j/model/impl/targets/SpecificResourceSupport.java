80f275bef2dd4da8b71ad019dfdda4b576494e1e
Emanuel Berndl <berndl@dimis.fim.uni-passau.de>
2015-12-18 10:35:33 +0100

    Fixed getTriples, which did not print the target side

--- anno4j-core/src/main/java/com/github/anno4j/model/impl/targets/SpecificResourceSupport.java
+++ anno4j-core/src/main/java/com/github/anno4j/model/impl/targets/SpecificResourceSupport.java
@@ -1,8 +1,10 @@
 package com.github.anno4j.model.impl.targets;
 
+import com.github.anno4j.model.AnnotationSupport;
 import com.github.anno4j.model.impl.ResourceObjectSupport;
 import com.github.anno4j.annotations.Partial;
 import org.apache.commons.io.IOUtils;
+import org.openrdf.repository.RepositoryException;
 import org.openrdf.rio.RDFFormat;
 import org.openrdf.rio.*;
 
@@ -11,7 +13,7 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
 @Partial
-public abstract class SpecificResourceSupport extends ResourceObjectSupport implements SpecificResource {
+public abstract class SpecificResourceSupport implements SpecificResource {
 
     @Override
     public String getTriples(RDFFormat format) {
@@ -21,7 +23,7 @@ public abstract class SpecificResourceSupport extends ResourceObjectSupport impl
         parser.setRDFHandler(writer);
 
         try {
-            super.getTriples(RDFFormat.NTRIPLES);
+            this.getObjectConnection().exportStatements(this.getResource(), null, null, true, writer);
 
             if (getSelector() != null) {
                 parser.parse(IOUtils.toInputStream(getSelector().getTriples(RDFFormat.NTRIPLES), "UTF-8"), "");
@@ -33,6 +35,8 @@ public abstract class SpecificResourceSupport extends ResourceObjectSupport impl
         } catch (RDFHandlerException e) {
             e.printStackTrace();
         } catch (RDFParseException e) {
+            e.printStackTrace();
+        } catch (RepositoryException e) {
             e.printStackTrace();
         }
 
