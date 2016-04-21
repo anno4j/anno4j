a9f4bcc5daf6266106a3a7caaf12e9a04e837a8d
Kai Schlegel <kai.schlegel@googlemail.com>
2016-04-21 11:07:40 +0200

    further transaction tests

--- anno4j-core/src/test/java/com/github/anno4j/transaction/TransactionTest.java
+++ anno4j-core/src/test/java/com/github/anno4j/transaction/TransactionTest.java
@@ -4,15 +4,22 @@ package com.github.anno4j.transaction;
 import com.github.anno4j.Anno4j;
 import com.github.anno4j.Transaction;
 import com.github.anno4j.model.Annotation;
+import com.github.anno4j.querying.GraphContextQueryTest;
+import com.github.anno4j.querying.QueryService;
 import org.junit.Before;
 import org.junit.Test;
+import org.openrdf.model.URI;
+import org.openrdf.model.impl.URIImpl;
 import org.openrdf.repository.RepositoryException;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class TransactionTest {
 
     private Anno4j anno4j;
+    private URI subgraph = new URIImpl("http://www.example.com/TESTGRAPH");
 
     @Before
     public void setUp() throws Exception {
@@ -37,4 +44,62 @@ public class TransactionTest {
 
         assertEquals(1, anno4j.findAll(Annotation.class).size());
     }
+
+    @Test
+    public void queryDuringTransactionTest() throws Exception {
+        // Create test annotation
+
+        Transaction transaction = anno4j.createTransaction();
+        transaction.begin();
+
+        transaction.createObject(Annotation.class);
+
+        assertEquals(1, transaction.findAll(Annotation.class).size());
+
+        transaction.commit();
+
+        assertEquals(1, transaction.findAll(Annotation.class).size());
+    }
+
+    @Test
+    public void roolbackTest() throws Exception {
+        Transaction transaction = anno4j.createTransaction();
+        transaction.begin();
+
+        transaction.createObject(Annotation.class);
+
+        assertEquals(1, transaction.findAll(Annotation.class).size());
+
+        transaction.rollback();
+
+        assertEquals(0, transaction.findAll(Annotation.class).size());
+    }
+
+    @Test
+    public void insertSubgraphTest() throws Exception {
+        Transaction transaction = anno4j.createTransaction();
+        transaction.begin();
+        transaction.setAllContexts(subgraph);
+
+        transaction.createObject(Annotation.class);
+        assertEquals(1, transaction.findAll(Annotation.class).size());
+        transaction.commit();
+
+        // check with new connection
+        assertEquals(1, anno4j.findAll(Annotation.class).size());
+    }
+
+    @Test
+    public void querySubgraphTest() throws Exception {
+        Transaction transaction = anno4j.createTransaction();
+        transaction.begin();
+        transaction.createObject(Annotation.class);
+        transaction.commit();
+
+        // check with new connection in subgraph
+        assertEquals(0, anno4j.findAll(Annotation.class, subgraph).size());
+        // check with new connection in global graph
+        assertEquals(1, anno4j.findAll(Annotation.class).size());
+    }
+
 }
