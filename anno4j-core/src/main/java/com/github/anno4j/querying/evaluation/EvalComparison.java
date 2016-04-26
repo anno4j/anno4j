a3330736dd983f76ab9fc5582f57828da9f7aec9
Andreas Eisenkolb <andreas.eisenkolb@gmail.com>
2016-04-26 12:24:08 +0200

    - Fixed equal filter method, using sparql filters - Added new comparison types: startsWith, endsWith and contains

--- anno4j-core/src/main/java/com/github/anno4j/querying/evaluation/EvalComparison.java
+++ anno4j-core/src/main/java/com/github/anno4j/querying/evaluation/EvalComparison.java
@@ -2,62 +2,41 @@ package com.github.anno4j.querying.evaluation;
 
 import com.github.anno4j.querying.Comparison;
 import com.github.anno4j.querying.Criteria;
-import com.hp.hpl.jena.graph.NodeFactory;
 import com.hp.hpl.jena.sparql.core.Var;
 import com.hp.hpl.jena.sparql.expr.*;
 import com.hp.hpl.jena.sparql.expr.nodevalue.NodeValueDouble;
 import com.hp.hpl.jena.sparql.syntax.ElementFilter;
 import com.hp.hpl.jena.sparql.syntax.ElementGroup;
-import org.apache.marmotta.ldpath.model.tests.LiteralTypeTest;
 
 /**
  * Created by schlegel on 03/06/15.
  */
 public class EvalComparison {
 
-    /**
-     * Evaluates the comparison method defined in the Criteria object.
-     *
-     * @param query        StringBuilder for the SPARQL query
-     * @param criteria     The current Criteria Object
-     * @param variableName The latest created variable name
-     */
-    public static void evaluate(StringBuilder query, Criteria criteria, String variableName) {
-
-        if (criteria.isNaN()) {
-            if (Comparison.EQ.equals(criteria.getComparison())) {
-                query
-                        .append("FILTER regex( str(?")
-                        .append(variableName)
-                        .append(") ")
-                        .append((criteria.isNaN()) ? ", \"" : ", ") // Adding quotes if the given value is not a number
-                        .append(criteria.getConstraint())
-                        .append((criteria.isNaN()) ? "\" ) ." : " ) ."); // Adding quotes if the given value is not a number
-            } else {
-                throw new IllegalStateException(criteria.getComparison() + " only allowed on Numbers.");
-            }
-        } else {
-            query
-                    .append("FILTER ( ?")
-                    .append(variableName)
-                    .append(" ")
-                    .append(criteria.getComparison().getSparqlOperator())
-                    .append(" ")
-                    .append(criteria.getConstraint())
-                    .append(" ) .");
-        }
-    }
-
     public static void evaluate(ElementGroup elementGroup, Criteria criteria, Var variable) {
+
         if (criteria.isNaN()) {
+
+            String constraint = "";
+
+            // Setting the boundaries (\b) to the RegExp, according to the comparison type
             if (Comparison.EQ.equals(criteria.getComparison())) {
-                ElementFilter filter = new ElementFilter(new E_Regex(new E_Str(new ExprVar(variable.asNode())), criteria.getConstraint(), ""));
-                elementGroup.addElementFilter(filter);
+                constraint = "^" + criteria.getConstraint() + "$";
+            } else if (Comparison.CONTAINS.equals(criteria.getComparison())) {
+                constraint = criteria.getConstraint();
+            } else if (Comparison.STARTS_WITH.equals(criteria.getComparison())) {
+                constraint = "^" + criteria.getConstraint();
+            } else if (Comparison.ENDS_WITH.equals(criteria.getComparison())) {
+                constraint = criteria.getConstraint() + "$";
             } else {
-                throw new IllegalStateException(criteria.getComparison() + " only allowed on Numbers.");
+                throw new IllegalStateException(criteria.getComparison() + " is only allowed on Numbers.");
+            }
+
+            if (!constraint.equals("")) {
+                elementGroup.addElementFilter(new ElementFilter(new E_Regex(new E_Str(new ExprVar(variable.asNode())), constraint , "")));
             }
         } else {
-            Expr expr = new E_Equals(new ExprVar(variable.asNode()), new ExprVar(criteria.getConstraint()));
+            Expr expr;
 
             if (criteria.getComparison().equals(Comparison.GT)) {
                 expr = new E_GreaterThan(new ExprVar(variable.asNode()), new NodeValueDouble(Double.parseDouble(criteria.getConstraint())));
@@ -67,10 +46,15 @@ public class EvalComparison {
                 expr = new E_LessThan(new ExprVar(variable.asNode()), new NodeValueDouble(Double.parseDouble(criteria.getConstraint())));
             } else if (criteria.getComparison().equals(Comparison.LTE)) {
                 expr = new E_LessThanOrEqual(new ExprVar(variable.asNode()), new NodeValueDouble(Double.parseDouble(criteria.getConstraint())));
+            } else if (criteria.getComparison().equals(Comparison.EQ)) {
+                expr = new E_Equals(new ExprVar(variable.asNode()), new ExprVar(criteria.getConstraint()));
+            } else {
+                throw new IllegalStateException(criteria.getComparison() + " is not allowed on Numbers.");
             }
 
-            ElementFilter filter = new ElementFilter(expr);
-            elementGroup.addElementFilter(filter);
+            if(expr != null) {
+                elementGroup.addElementFilter(new ElementFilter(expr));
+            }
         }
     }
 }
