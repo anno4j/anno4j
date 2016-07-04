884c7dd66ce6e5bff3aeef4667ba27864e898a24
Emanuel Berndl <berndl@dimis.fim.uni-passau.de>
2016-07-04 13:24:30 +0200

    - Adapted Choice to new spec, added Target/Body Interface for inheritance - Added test method for body/target level

--- anno4j-core/src/test/java/com/github/anno4j/model/impl/multiplicity/ChoiceTest.java
+++ anno4j-core/src/test/java/com/github/anno4j/model/impl/multiplicity/ChoiceTest.java
@@ -2,6 +2,7 @@ package com.github.anno4j.model.impl.multiplicity;
 
 import com.github.anno4j.Anno4j;
 import com.github.anno4j.model.Annotation;
+import com.github.anno4j.model.impl.ResourceObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.openrdf.repository.RepositoryException;
@@ -35,5 +36,32 @@ public class ChoiceTest {
         List<Choice> result = this.anno4j.findAll(Choice.class);
 
         assertEquals(1, result.size());
+    }
+
+    /**
+     * Tests, if the body and target functionality of Choice works
+     */
+    @Test
+    public void choiceBodyTargetTest() throws RepositoryException, IllegalAccessException, InstantiationException {
+        Annotation annotation = this.anno4j.createObject(Annotation.class);
+
+        Choice choiceBody = this.anno4j.createObject(Choice.class);
+        choiceBody.addItem(this.anno4j.createObject(ResourceObject.class));
+
+        Choice choiceTarget = this.anno4j.createObject(Choice.class);
+        choiceTarget.addItem(this.anno4j.createObject(ResourceObject.class));
+        choiceTarget.addItem(this.anno4j.createObject(ResourceObject.class));
+
+        annotation.addTarget(choiceTarget);
+        annotation.setBody(choiceBody);
+
+        List<Annotation> result = this.anno4j.findAll(Annotation.class);
+
+        assertEquals(1, result.size());
+
+        Annotation resultAnnotation = result.get(0);
+
+        assertEquals(1, ((Choice) resultAnnotation.getBody()).getItems().size());
+        assertEquals(2, ((Choice) resultAnnotation.getTarget().toArray()[0]).getItems().size());
     }
 }
