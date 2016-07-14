2a972abef1fde14733893f6d7c07c38dba3ec337
Emanuel Berndl <berndl@dimis.fim.uni-passau.de>
2016-07-14 13:38:58 +0200

    Removed "abstract" modifier for MotivationFactory, not needed

--- anno4j-core/src/main/java/com/github/anno4j/model/MotivationFactory.java
+++ anno4j-core/src/main/java/com/github/anno4j/model/MotivationFactory.java
@@ -5,7 +5,7 @@ import com.github.anno4j.model.namespaces.OADM;
 import org.openrdf.model.impl.URIImpl;
 import org.openrdf.repository.RepositoryException;
 
-public abstract class MotivationFactory {
+public class MotivationFactory {
 
     public static Motivation getBookmarking(Anno4j anno4j) throws RepositoryException, IllegalAccessException, InstantiationException {
         return anno4j.createObject(Motivation.class, new URIImpl(OADM.MOTIVATION_BOOKMARKING));
