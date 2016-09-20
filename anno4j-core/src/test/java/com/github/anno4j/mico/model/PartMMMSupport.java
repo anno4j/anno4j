d133c6ed080dc2023f8a94b7f0f62cd568ba8f5b
Thomas Weißgerber <weissger@posteo.de>
2016-09-20 15:04:16 +0200

    revert InvocationMessageContext add simple getter setter as fix???

--- anno4j-core/src/test/java/com/github/anno4j/mico/model/PartMMMSupport.java
+++ anno4j-core/src/test/java/com/github/anno4j/mico/model/PartMMMSupport.java
@@ -1,14 +1,31 @@
 package com.github.anno4j.mico.model;
 
 import com.github.anno4j.annotations.Partial;
+import com.github.anno4j.mico.namespace.MMM;
 import com.github.anno4j.model.AnnotationSupport;
 import com.github.anno4j.model.Target;
+import org.openrdf.annotations.Iri;
+
+import java.util.Set;
 
 /**
  * Support class for the Part.
  */
 @Partial
 public abstract class PartMMMSupport extends AnnotationSupport implements PartMMM {
+
+    @Override
+    public Set<Target> getTarget() {
+        return target;
+    }
+
+    @Override
+    public void setTarget(Set<Target> target) {
+        this.target = target;
+    }
+
+    @Iri(MMM.HAS_TARGET)
+    private Set<Target> target;
 
     @Override
     public void addTarget(Target target) {
