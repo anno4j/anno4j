43f53596f62881d214282bf36b92f6b6f74c1be6
Emanuel Berndl <berndl@dimis.fim.uni-passau.de>
2016-09-28 10:00:49 +0200

    Fixed issue with deliverable interface

--- anno4j-core/src/test/java/com/github/anno4j/mico/model/AnimalDetectionBody.java
+++ anno4j-core/src/test/java/com/github/anno4j/mico/model/AnimalDetectionBody.java
@@ -11,10 +11,8 @@ import org.openrdf.annotations.Iri;
 @Iri(MMMTERMS.ANIMAL_DETECTION_BODY)
 public interface AnimalDetectionBody extends BodyMMM {
 
-    @Iri(MMM.HAS_CONFIDENCE)
     void setConfidence(Double confidence);
 
-    @Iri(MMM.HAS_CONFIDENCE)
     Double getConfidence();
 
     @Iri(RDF.VALUE)
