c352cf2d947310e1b0e01a9c727e731d24c8743e
Emanuel Berndl <berndl@dimis.fim.uni-passau.de>
2016-10-25 09:26:24 +0200

    Bump to version 2.2.0.MICO-SNAPSHOT

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,6 +8,29 @@ if [ "${TRAVIS_PULL_REQUEST}" == "false" ] && [ "${TRAVIS_BRANCH}" == "develop" 
 elif [ "${TRAVIS_PULL_REQUEST}" == "false" ] && [ "${TRAVIS_BRANCH}" == "develop-mico" ]; then
    echo "Deploy develop snapshot of Anno4j MICO:" $COMMITMSG ;
    mvn clean deploy --settings settings.xml
+elif [ "${TRAVIS_PULL_REQUEST}" == "false" ] && [ "${TRAVIS_BRANCH}" == "master-mico" ]; then
+    if [[ $COMMITMSG != \[maven-release-plugin\]* ]]; then
+        echo "Release Anno4j:" $COMMITMSG
+        echo "Modify git configuration"
+        git config --global user.email "anno4j@web.de"
+        git config --global user.name "Anno4jCI"
+        echo "Add github remote repository"
+        git remote rm origin
+        git remote add origin https://Anno4jCI:$GITHUB_PW@github.com/anno4j/anno4j.git
+        echo "Checkout master-mico branch"
+        git checkout master-mico || git checkout -b master-mico
+        echo "Decrypt gpg keyrings"
+        openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in keyrings/secring.gpg.enc -out keyrings/local.secring.gpg -d
+        openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in keyrings/pubring.gpg.enc -out keyrings/local.pubring.gpg -d
+        echo "Set permissions"
+        ls -al keyrings
+        chmod a+rx keyrings/local.pubring.gpg
+        chmod a+rx keyrings/local.secring.gpg
+        ls -al keyrings
+        mvn clean release:prepare release:perform -DignoreSnapshots=true -B --settings settings.xml
+    else
+        echo "Skip TravisCI commit:" $COMMITMSG
+    fi
 elif [ "${TRAVIS_PULL_REQUEST}" == "false" ] && [ "${TRAVIS_BRANCH}" == "master" ]; then
     if [[ $COMMITMSG != \[maven-release-plugin\]* ]]; then
         echo "Release Anno4j:" $COMMITMSG
