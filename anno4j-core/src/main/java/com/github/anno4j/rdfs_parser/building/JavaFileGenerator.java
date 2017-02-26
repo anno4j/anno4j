6940214e6e40953de68b37df879a515e2fb42fa8
fischmat <mail@matthias-fisch.de>
2017-02-26 18:14:12 +0100

    Fixed exception problem.

--- anno4j-core/src/main/java/com/github/anno4j/rdfs_parser/building/JavaFileGenerator.java
+++ anno4j-core/src/main/java/com/github/anno4j/rdfs_parser/building/JavaFileGenerator.java
@@ -3,17 +3,16 @@ package com.github.anno4j.rdfs_parser.building;
 import com.github.anno4j.Anno4j;
 import com.github.anno4j.model.namespaces.RDF;
 import com.github.anno4j.model.namespaces.RDFS;
+import com.github.anno4j.model.namespaces.XSD;
 import com.github.anno4j.rdfs_parser.model.ExtendedRDFSClazz;
-import com.github.anno4j.rdfs_parser.naming.IdentifierBuilder;
 import com.squareup.javapoet.JavaFile;
 import org.openrdf.repository.RepositoryException;
 import org.openrdf.repository.config.RepositoryConfigException;
 
 import java.io.*;
-import java.net.URISyntaxException;
 
 /**
- *
+ * Generates Anno4j Java classes for an RDFS ontology.
  */
 public class JavaFileGenerator {
 
@@ -40,64 +39,41 @@ public class JavaFileGenerator {
         modelBuilder.addRDF(rdfData, baseUri);
     }
 
-    private File getPackageDirectory(File outputDir, String packageName) throws JavaFileGenerationException {
-        String path = outputDir.getAbsolutePath() + File.separator + packageName.replace(".", File.separator);
-        File packageDirectory = new File(path);
-        if(packageDirectory.isDirectory() || packageDirectory.mkdirs()) {
-            return packageDirectory;
-        } else {
-            throw new JavaFileGenerationException("Could not create directory for package " + packageName);
-        }
-    }
-
-    public void generate(OntGenerationConfig config) throws JavaFileGenerationException, RDFSModelBuilder.RDFSModelBuildingException {
+    private File createOutputDirectory(OntGenerationConfig config) throws JavaFileGenerationException {
         File outputDirectory = config.getOutputDirectory();
-        if(!outputDirectory.exists()) {
-            if(!outputDirectory.mkdirs()) {
+        String[] outputDirectoryContent = outputDirectory.list();
+
+        if (!outputDirectory.exists()) {
+            if (!outputDirectory.mkdirs()) {
                 throw new JavaFileGenerationException("Could not create directory " + outputDirectory.getAbsolutePath());
             }
-        } else if(outputDirectory.isDirectory() && outputDirectory.list().length != 0) {
+        } else if (outputDirectory.isDirectory() && outputDirectoryContent != null && outputDirectoryContent.length != 0) {
             throw new JavaFileGenerationException("The output directory " + outputDirectory.getAbsolutePath() + " is not empty.");
         }
+        return outputDirectory;
+    }
+
+    public void generate(OntGenerationConfig config) throws JavaFileGenerationException, RDFSModelBuilder.RDFSModelBuildingException, IOException {
+        File outputDirectory = createOutputDirectory(config);
 
         modelBuilder.build();
 
-        if(!modelBuilder.validate().isValid()) {
+        if (!modelBuilder.validate().isValid()) {
             throw new JavaFileGenerationException("The built model is invalid.");
         }
 
         for (ExtendedRDFSClazz clazz : modelBuilder.getRDFSClazzes()) {
-            // Do not generate files for RDF(S) classes:
-            if(!clazz.getResourceAsString().startsWith(RDF.NS) && !clazz.getResourceAsString().startsWith(RDFS.NS)) {
-                try {
-                    String packageName = clazz.getJavaPackageName();
+            // Do not generate files for RDF(S) classes and XSD datatypes:
+            if (!clazz.getResourceAsString().startsWith(RDF.NS) && !clazz.getResourceAsString().startsWith(RDFS.NS)
+                    && !clazz.getResourceAsString().startsWith(XSD.NS)) {
+                String packageName = clazz.getJavaPackageName();
 
-                    JavaFile javaFile = JavaFile.builder(packageName, clazz.buildTypeSpec(config)).build();
-                    javaFile.writeTo(outputDirectory);
+                JavaFile interfaceFile = JavaFile.builder(packageName, clazz.buildTypeSpec(config)).build();
+                JavaFile supportFile = JavaFile.builder(packageName, clazz.buildSupportTypeSpec(config)).build();
 
-                } catch ( URISyntaxException | IOException e) {
-                    throw new JavaFileGenerationException("Failed generating a name for " + clazz.getResourceAsString()
-                            + " Details: " + e.getMessage());
-                }
+                interfaceFile.writeTo(outputDirectory);
+                supportFile.writeTo(outputDirectory);
             }
-        }
-        System.out.println("Files written to " + outputDirectory.getAbsolutePath());
-    }
-
-    public static void main(String[] args) throws RepositoryException, RepositoryConfigException, FileNotFoundException {
-        String[] langPref = {"de", "en"};
-        OntGenerationConfig config = new OntGenerationConfig();
-        config.setJavaDocLanguagePreference(langPref);
-        config.setIdentifierLanguagePreference(langPref);
-        config.setOutputDirectory(new File("/Users/fischmat/Desktop/gen"));
-
-        JavaFileGenerator generator = new JavaFileGenerator();
-        generator.addRDF(new FileInputStream("/Users/fischmat/Desktop/testont.xml"), "http://example.de/");
-
-        try {
-            generator.generate(config);
-        } catch (JavaFileGenerationException | RDFSModelBuilder.RDFSModelBuildingException e) {
-            e.printStackTrace();
         }
     }
 }
