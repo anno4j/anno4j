952b55fd59087bbb7c104fda80fc6672315d7e1d
fischmat <mail@matthias-fisch.de>
2017-04-04 16:52:03 +0200

    Removed package schema_parsing with old parsing approach.

--- anno4j-core/src/main/java/com/github/anno4j/schema_parsing/SchemaParser.java
+++
@@ -1,7 +0,0 @@
-package com.github.anno4j.schema_parsing;
-
-/**
- * Created by Manu on 02/11/16.
- */
-public class SchemaParser {
-}
--- anno4j-core/src/main/java/com/github/anno4j/schema_parsing/model/Schema.java
+++
@@ -1,9 +0,0 @@
-package com.github.anno4j.schema_parsing.model;
-
-/**
- * Created by Manu on 18/11/16.
- */
-public enum Schema {
-
-    OWL, RDFS
-}
