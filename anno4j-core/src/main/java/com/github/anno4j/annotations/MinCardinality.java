e87808f7908d8c73c9412e5b512cfa3e79d597b9
Matthias Fisch <mail@matthias-fisch.de>
2017-04-15 17:25:04 +0200

    Added "onClass" parameter.

--- anno4j-core/src/main/java/com/github/anno4j/annotations/MinCardinality.java
+++ anno4j-core/src/main/java/com/github/anno4j/annotations/MinCardinality.java
@@ -1,5 +1,8 @@
 package com.github.anno4j.annotations;
 
+import com.github.anno4j.model.impl.ResourceObject;
+import com.github.anno4j.schema.model.owl.OWLClazz;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -17,4 +20,11 @@ public @interface MinCardinality {
      * @return The minimum number of values a property must have.
      */
     int value();
+
+    /**
+     * The class on which the restriction is imposed.
+     * owl:Class is allowed to make the restriction unqualified.
+     * @return The class on which this restriction is imposed.
+     */
+    Class<? extends ResourceObject> onClass() default OWLClazz.class;
 }
