647d4a9a447722cd93303f7fd82e9988c82c7dd6
Matthias Fisch <matthias-fisch@hotmail.de>
2018-01-22 13:37:53 +0100

    Remove backed up files after test

--- anno4j-core/src/test/java/com/github/anno4j/persisting/PersistingTest.java
+++ anno4j-core/src/test/java/com/github/anno4j/persisting/PersistingTest.java
@@ -9,7 +9,6 @@ import org.openrdf.repository.object.ObjectConnection;
 import org.openrdf.repository.object.composition.ClassFactory;
 import org.openrdf.repository.sail.SailRepository;
 import org.openrdf.sail.memory.MemoryStore;
-import static org.junit.Assert.assertNotNull;
 
 import java.io.File;
 import java.io.IOException;
@@ -17,9 +16,14 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Tests the customized class loader of Alibaba.
+ */
 public class PersistingTest {
 
-    private static final String CLASS_BACKUP_DIR = System.getProperty("user.home");
+    private static final String USER_HOME = System.getProperty("user.home") + File.separator;
 
     private void addClassLoaderLookup(ObjectConnection connection, String path) throws MalformedURLException {
         ClassFactory classFactory = connection.getObjectFactory().getResolver().getClassFactory();
@@ -34,18 +38,32 @@ public class PersistingTest {
         FileUtils.copyDirectory(classFactory.getOutput(), targetDir);
     }
 
+    private void removeDirectory(File directory) throws IOException {
+        FileUtils.deleteDirectory(directory);
+    }
+
     @Test
     public void testPresistence() throws Exception {
         // Create an Anno4j instacne without persisting schema annotations:
         Anno4j anno4j = new Anno4j(new SailRepository(new MemoryStore()), new IDGeneratorAnno4jURN(), null, false);
 
-        // Set own directory as preferred lookup locationf or class loader:
+        // Set own directory as preferred lookup location for class loader:
         ObjectConnection connection = anno4j.getObjectRepository().getConnection();
-        addClassLoaderLookup(connection, CLASS_BACKUP_DIR);
+        addClassLoaderLookup(connection, USER_HOME + "object");
 
+        // Generate bytecode for Annotation:
         Annotation annotation = anno4j.createObject(Annotation.class);
         assertNotNull(annotation);
 
-        backupClassFiles(connection, CLASS_BACKUP_DIR);
+        // Backup and delete bytecode:
+        backupClassFiles(connection, USER_HOME);
+        removeDirectory(connection.getObjectFactory().getResolver().getClassFactory().getOutput());
+
+        // Create Annotation again. Should be loaded from backed up directory:
+        annotation = anno4j.createObject(Annotation.class);
+        assertNotNull(annotation);
+
+        // Clean up:
+        removeDirectory(new File(USER_HOME + "object"));
     }
 }
