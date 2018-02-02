307013eb3745380c242652872122fa4ca08bf8b6
Emanuel Berndl <berndl@dimis.fim.uni-passau.de>
2018-02-02 19:44:09 +0100

    Initial Commit

---
+++ anno4jrest/src/main/java/com/github/anno4j/Anno4jRestApplication.java
@@ -0,0 +1,12 @@
+package com.github.anno4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Anno4jRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Anno4jRestApplication.class, args);
+	}
+}
---
+++ anno4jrest/src/main/java/com/github/anno4j/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.github.anno4j;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Anno4jRestApplication.class);
+	}
+
+}
